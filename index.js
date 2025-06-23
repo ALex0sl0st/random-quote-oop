5a97cb974a1b44068a92d04993fe754ed66426d1
ALex0sl0st <alex0sl0st@gmail.com>
2025-06-23 14:37:47 +0300

    Add button for fetching random quotes via API

--- index.js
+++ index.js
@@ -5,27 +5,38 @@ import currentQuoteManager from "./src/classes/storage/CurrentQuoteManager.js";
 import { appUI } from "./src/classes/AppUI.js";
 import { initializeApp } from "./src/initializeApp.js";
 
-const nextQuoteBtn = document.getElementById("next-quote-btn");
+const localQuoteBtn = document.getElementById("local-quote-btn");
+const onlineQuoteBtn = document.getElementById("online-quote-btn");
 const starElement = document.getElementById("star");
 
 initializeApp({ quotes, starElement });
 
-function setRandomQuote() {
-  const randomQuote = QuoteProvider.getRandomQuoteFromArray(quotes);
-
-  currentQuoteManager.update(randomQuote);
+function setCurrentQuote(quote) {
+  currentQuoteManager.update(quote);
   appUI.displayCurrentQuote(currentQuoteManager.getCurrentQuote(), starElement);
-  // console.log(
-  //   "First",
-  //   quotes.filter((el) => isQuoteFavorite(el.id))
-  // );
 }
 
-nextQuoteBtn.addEventListener("click", setRandomQuote);
+function setLocalRandomQuote() {
+  const randomQuote = QuoteProvider.getRandomQuoteFromArray(quotes);
+  // console.log("local");
+
+  setCurrentQuote(randomQuote);
+}
+
+async function setOnlineRandomQuote() {
+  const randomQuote = await QuoteProvider.getRandomQuoteViaAPI();
+  // console.log("api");
+
+  if (randomQuote) setCurrentQuote(randomQuote);
+}
+
+localQuoteBtn.addEventListener("click", setLocalRandomQuote);
+onlineQuoteBtn.addEventListener("click", setOnlineRandomQuote);
 starElement.addEventListener("click", () => {
   toggleFavoriteQuote(starElement);
 });
 
-// function getCurrentQuoteId() {
-//   return currentQuoteId;
-// }
+// console.log(
+//   "First",
+//   quotes.filter((el) => isQuoteFavorite(el.id))
+// );
--- src/classes/QuoteProvider.js
+++ src/classes/QuoteProvider.js
@@ -19,6 +19,23 @@ class QuoteProvider {
   static setLastRandomIndex(index) {
     QuoteProvider.lastRandomIndex = index;
   }
+
+  static async getRandomQuoteViaAPI(
+    url = "https://quoteslate.vercel.app/api/quotes/random"
+  ) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      const { id, quote: text, author } = data;
+      return Quote.createFromObject({ id, text, author });
+    } catch (error) {
+      // console.log("CUSTOM ERROR");
+      console.error(error);
+
+      return null;
+    }
+  }
 }
 
 export default QuoteProvider;

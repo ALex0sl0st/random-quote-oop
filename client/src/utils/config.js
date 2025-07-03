518784480d04a9dd9a56946ce5c25f710c116b97
ALex0sl0st <alex0sl0st@gmail.com>
2025-07-03 14:41:32 +0300

    Change LOCAL_API_URL to my public API URL

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,5 +1,6 @@
 const PUBLIC_API_URL = "https://quoteslate.vercel.app";
-const LOCAL_API_URL = "http://localhost:3000";
+const LOCAL_API_URL = "https://random-quote-oop-6qt4.onrender.com";
+// https://random-quote-oop-6qt4.onrender.com;  http://localhost:3000
 
 const config = { PUBLIC_API_URL, LOCAL_API_URL };
 
